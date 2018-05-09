d944ae80f53c2fc346b8b8d087e7c26c1d65f85c
IDmikael <latmikael@gmail.com>
2018-05-09 20:51:25 +0300

    Reload icons again

--- flags-iso/src/main/java/com/idmikael/flags_iso/FlagsIso.java
+++ flags-iso/src/main/java/com/idmikael/flags_iso/FlagsIso.java
@@ -8,7 +8,16 @@ public class FlagsIso {
 
     public static Drawable getFlagDrawable(Context context, String flagName) {
         Resources res =  context.getResources();
-        int resourceId = res.getIdentifier(flagName.toLowerCase(), "drawable", context.getPackageName());
+        int resourceId = res.getIdentifier(checkFlagName(flagName.toLowerCase()), "drawable", context.getPackageName());
         return res.getDrawable(resourceId);
+    }
+
+    private static String checkFlagName(String flagName){
+        if (flagName.equals("do"))
+            return "dom";
+        else if (flagName.contains("-"))
+            return flagName.replaceAll("-", "_");
+        else
+            return flagName;
     }
 }
